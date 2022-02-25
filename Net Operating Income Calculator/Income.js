2ce9864168a5856cb0b52612361cd12a88c43bc0
NTCG25 <65662881+NTCG25@users.noreply.github.com>
2022-02-25 11:28:49 -0600

    Add files via upload

---
+++ Net Operating Income Calculator/Income.js
@@ -0,0 +1,4 @@
+const grossIncome = prompt("What is your gross Income?", 0); 
+const operatingExpenses = prompt("What is your operating expenses?", 0)
+
+alert(`Your NOI is ${grossIncome - operatingExpenses}`)
